6663d72906b5d886f0c376d58ecda91da0891fe3
AbirHamzi <abir.hamzi@etudiant-isi.utm.tn>
2019-06-09 19:04:06 +0100

    change Dares from string[] to mapping && Documentation

--- contracts/game.sol
+++ contracts/game.sol
@@ -2,30 +2,42 @@ pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
 contract game{
+    //Struct that represents a player/watcher
     struct Player {
-        string amountBet;
+        string nickName;
     }  
-    mapping(address => Player) public playerInfo;
-    
-    address payable[] public watchers;
-    //mapping (uint => string) Dares;
-    string[] Dares;
+    mapping(address => Player) public playerInfo; // maaping(user-address => user-informations)
+    address[] public players; // players are stored in blockchain as an array of adresses
+    address payable[] public watchers; // watchers are stored in blockchain as an array of payable adresses
+    mapping (uint => string) Dares; // mapping(Dare-key => Dare-description)
+    uint[] DaresKeyList; //array that store all Dares keys 
+
     event transferFund(address _sender,address _reciever,uint _amount);
+    
 
     constructor()public {
-        Dares.push("Eat something and then talk with your mouth full");
-        Dares.push("Fill your mouth with water and try singing a song");
-        Dares.push("Call up your crush and declare your love for him");
-        Dares.push("Make an obscene phone call to a random number");
+        //Hard-coded list of dares for test
+        Dares[1]="Eat something and then talk with your mouth full";
+        DaresKeyList.push(1);
+        Dares[2]="Fill your mouth with water and try singing a song";
+        DaresKeyList.push(2);
+        Dares[3]="Call up your crush and declare your love for him";
+        DaresKeyList.push(3);
+        Dares[4]="Make an obscene phone call to a random number";
+        DaresKeyList.push(4);
+
     }
   
+  /// @dev function that returns all dares 
+  /// @return array of string new experimental blockchain feature 
     function getDares()public view returns(string[] memory){
-      string[] memory _Dares = new string[](Dares.length);
-       for(uint i = 0; i < Dares.length; i++){
-          _Dares[i] = Dares[i];
+      string[] memory _Dares = new string[](DaresKeyList.length);
+       for(uint i = 0; i < DaresKeyList.length; i++){
+           _Dares[i] = Dares[DaresKeyList[i]];
        }
        return _Dares;
     }
+  
     function getBalance() public view returns(uint){
          return (msg.sender.balance);
     }
